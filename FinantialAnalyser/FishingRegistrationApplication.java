5a6fd035c2dbbb8460c691c22814006db910ec08
Thareesha <thareesha98>
2025-06-03 00:55:04 +0530

    created apis

---
+++ FinantialAnalyser/FishingRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.spiral.FishingRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingRegistrationApplication.class, args);
+	}
+
+}
